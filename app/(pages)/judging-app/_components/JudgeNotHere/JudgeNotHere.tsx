0e9fec87c14ee956bd4be41f31ab71e5e1aa2d08
Win Cheng <winzamark12@gmail.com>
2024-01-10 09:43:26 -0800

    feat(judge-not-here): used moment.js for timer api

--- app/(pages)/judging-app/_components/JudgeNotHere/JudgeNotHere.tsx
+++ app/(pages)/judging-app/_components/JudgeNotHere/JudgeNotHere.tsx
@@ -1,6 +1,26 @@
+'use client';
 // import Image from 'next/image';
 import styles from './JudgeNotHere.module.scss';
+import moment from 'moment';
+import { useEffect, useState } from 'react';
+
+const useTimer = () => {
+  const [time, setTime] = useState(moment());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(moment());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return time.format('mm:ss'); // Format time as needed
+};
+
 export default function JudgeNotHere() {
+  const curTime = useTimer();
+
   return (
     <div className={styles.judgeNotHere}>
       <div className={styles.container}>
@@ -11,8 +31,9 @@ export default function JudgeNotHere() {
             once when the time expires!
           </p>
         </div>
-        <div className="styles container_timer">
-          <h1>Timer HERE</h1>
+        <div className={styles.container_timer}>
+          {/* <div className={styles.container_timer_mins}>{curTime}</div> */}
+          <h1>{curTime}</h1>
         </div>
       </div>
     </div>
