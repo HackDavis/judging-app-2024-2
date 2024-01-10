fd5333fc3ef070a6a73d038b491d27bcfd07e129
Win Cheng <winzamark12@gmail.com>
2024-01-10 10:33:16 -0800

    feat(judge-not-here): timer complete, finishing up the component

--- app/(pages)/judging-app/_components/JudgeNotHere/JudgeNotHere.tsx
+++ app/(pages)/judging-app/_components/JudgeNotHere/JudgeNotHere.tsx
@@ -15,12 +15,20 @@ const useTimer = () => {
     return () => clearInterval(interval);
   }, []);
 
-  return time.format('mm:ss'); // Format time as needed
+  // return time.format('mm:ss'); // Format time as needed
+  return time;
 };
 
 export default function JudgeNotHere() {
   const curTime = useTimer();
 
+  // Ensuring two digits for minutes and seconds
+  const minutesString = curTime.minutes().toString().padStart(2, '0');
+  const secondsString = curTime.seconds().toString().padStart(2, '0');
+
+  // Splitting into individual digits
+  const minuteDigits = minutesString.split('');
+  const secondDigits = secondsString.split('');
   return (
     <div className={styles.judgeNotHere}>
       <div className={styles.container}>
@@ -33,7 +41,23 @@ export default function JudgeNotHere() {
         </div>
         <div className={styles.container_timer}>
           {/* <div className={styles.container_timer_mins}>{curTime}</div> */}
-          <h1>{curTime}</h1>
+          <div className={styles.container_timer_card}>
+            <h1>{minuteDigits[0]}</h1>
+          </div>
+          <div className={styles.container_timer_card}>
+            <h1>{minuteDigits[1]}</h1>
+          </div>
+          {/*  */}
+          <div className={styles.container_timer_colon}>
+            <h1>:</h1>
+          </div>
+          {/*  */}
+          <div className={styles.container_timer_card}>
+            <h1>{secondDigits[0]}</h1>
+          </div>
+          <div className={styles.container_timer_card}>
+            <h1>{secondDigits[1]}</h1>
+          </div>
         </div>
       </div>
     </div>
