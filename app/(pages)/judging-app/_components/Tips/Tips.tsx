a27ea5001a0c7897450b826defc95de3c56dab18
Sauvikesh <81947858+Sauvikesh@users.noreply.github.com>
2024-01-11 16:45:58 -0800

    feat: created progress bar that moves on arrow click

--- app/(pages)/judging-app/_components/Tips/Tips.tsx
+++ app/(pages)/judging-app/_components/Tips/Tips.tsx
@@ -1,18 +1,23 @@
 'use client';
-
 import { useState } from 'react';
 import styles from './Tips.module.scss';
 
 export default function Tips() {
   const [scrollPosition, setScrollPosition] = useState(0);
+  const maxPosition = 4;
+  const smallestPosition = 0;
 
-  const progressBarValues = [
-    'M3 2.99998H48',
-    'M46 2.99998H91',
-    'M91 3H136',
-    'M136 3H181',
-    'M183 3H228',
-  ];
+  const moveRight = () => {
+    if (scrollPosition + 1 <= maxPosition) {
+      setScrollPosition(scrollPosition + 1);
+    }
+  };
+
+  const moveLeft = () => {
+    if (scrollPosition - 1 >= smallestPosition) {
+      setScrollPosition(scrollPosition - 1);
+    }
+  };
 
   return (
     <div id={styles.tipSection}>
@@ -33,44 +38,69 @@ export default function Tips() {
       </div>
 
       <div id={styles.scrollBar}>
-        <LeftArrow />
-        <Bar scrollPosition={progressBarValues[scrollPosition]} />
-        <RightArrow />
+        <div onClick={moveLeft}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="35"
+            height="35"
+            viewBox="0 0 35 35"
+            fill="none"
+          >
+            <circle cx="17.5" cy="17.3259" r="17" fill="#005271" />
+            <circle cx="17.5" cy="17.3259" r="17" fill="#005271" />
+            <path
+              d="M19.5 12.3259L14.5 16.6116L19.5 21.3259"
+              stroke="#9EE7E5"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
+        <Bar translatePosition={scrollPosition * 45} />
+        <div onClick={moveRight}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="35"
+            height="35"
+            viewBox="0 0 35 35"
+            fill="none"
+          >
+            <circle
+              cx="17"
+              cy="17"
+              r="17"
+              transform="matrix(-1 0 0 1 34.5 0.325867)"
+              fill="#005271"
+            />
+            <path
+              d="M15.5 12.3259L20.5 16.6116L15.5 21.3259"
+              stroke="#9EE7E5"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
       </div>
     </div>
   );
 }
 
-const Bar = (props: { scrollPosition: string }) => {
+const Bar = (props: { translatePosition: number }) => {
   return (
-    <div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="230"
-        height="6"
-        viewBox="0 0 230 6"
-        fill="none"
-      >
-        <path
-          d="M2.5 3.32587L227.5 3.32589"
-          stroke="#E0DFD8"
-          strokeWidth="5"
-          strokeLinecap="round"
-        />
-        <path
-          d={props.scrollPosition}
-          stroke="#1FBBC0"
-          strokeWidth="5"
-          strokeLinecap="round"
-        />
-      </svg>
+    <div id={styles.bar}>
+      <div
+        id={styles.progress}
+        style={{ transform: `translateX(${props.translatePosition}px)` }}
+      ></div>
     </div>
   );
 };
 
-const LeftArrow = () => {
+const LeftArrow = (props: { moveLeft: any }) => {
   return (
-    <div>
+    <div onClick={props.moveLeft}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="35"
