86ff8bdad90022e1f92c99e98d1aae5e58e3f3c7
Sauvikesh <81947858+Sauvikesh@users.noreply.github.com>
2024-01-14 22:01:43 -0800

    style/feat: updated class names and css structure. added some responsiveness

--- app/(pages)/judging-app/_components/Tips/Tips.tsx
+++ app/(pages)/judging-app/_components/Tips/Tips.tsx
@@ -28,64 +28,66 @@ export default function Tips() {
   };
 
   return (
-    <div id={styles.tipSection}>
-      <div id={styles.tipsExplained}>
-        <p id={styles.tipsHeader}>Some tips!</p>
-        <p id={styles.tipsSubHeader}>
-          Whether you're a first time hacker or a hacking veteran, here are some
-          useful tips for everyone!
-        </p>
-      </div>
-
-      <div id={styles.tips}>
-        <p className={styles.tipText}>Tip #{scrollPosition + 1}:</p>
-        <p className={styles.tipText}>{tipsArray[scrollPosition]}</p>
-      </div>
-
-      <div id={styles.scrollBar}>
-        <div onClick={moveLeft}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="35"
-            height="35"
-            viewBox="0 0 35 35"
-            fill="none"
-          >
-            <circle cx="17.5" cy="17.3259" r="17" fill="#005271" />
-            <circle cx="17.5" cy="17.3259" r="17" fill="#005271" />
-            <path
-              d="M19.5 12.3259L14.5 16.6116L19.5 21.3259"
-              stroke="#9EE7E5"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
+    <div className={styles.container}>
+      <div className={styles.subContainer}>
+        <div className={styles.tipsContainer}>
+          <p className={styles.tipsHeader}>Some tips!</p>
+          <p className={styles.tipsSubHeader}>
+            Whether you're a first time hacker or a hacking veteran, here are
+            some useful tips for everyone!
+          </p>
         </div>
-        <Bar translatePosition={scrollPosition * 45} />
-        <div onClick={moveRight}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="35"
-            height="35"
-            viewBox="0 0 35 35"
-            fill="none"
-          >
-            <circle
-              cx="17"
-              cy="17"
-              r="17"
-              transform="matrix(-1 0 0 1 34.5 0.325867)"
-              fill="#005271"
-            />
-            <path
-              d="M15.5 12.3259L20.5 16.6116L15.5 21.3259"
-              stroke="#9EE7E5"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
+
+        <div className={styles.tipsContent}>
+          <p className={styles.tipText}>Tip #{scrollPosition + 1}:</p>
+          <p className={styles.tipText}>{tipsArray[scrollPosition]}</p>
+        </div>
+
+        <div className={styles.progressBarContainer}>
+          <div onClick={moveLeft} className={styles.arrow}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="35"
+              height="35"
+              viewBox="0 0 35 35"
+              fill="none"
+            >
+              <circle cx="17.5" cy="17.3259" r="17" fill="#005271" />
+              <circle cx="17.5" cy="17.3259" r="17" fill="#005271" />
+              <path
+                d="M19.5 12.3259L14.5 16.6116L19.5 21.3259"
+                stroke="#9EE7E5"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+          <Bar translatePosition={scrollPosition * 45} />
+          <div onClick={moveRight} className={styles.arrow}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="35"
+              height="35"
+              viewBox="0 0 35 35"
+              fill="none"
+            >
+              <circle
+                cx="17"
+                cy="17"
+                r="17"
+                transform="matrix(-1 0 0 1 34.5 0.325867)"
+                fill="#005271"
+              />
+              <path
+                d="M15.5 12.3259L20.5 16.6116L15.5 21.3259"
+                stroke="#9EE7E5"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
         </div>
       </div>
     </div>
@@ -94,63 +96,11 @@ export default function Tips() {
 
 const Bar = (props: { translatePosition: number }) => {
   return (
-    <div id={styles.bar}>
+    <div className={styles.bar}>
       <div
-        id={styles.progress}
+        className={styles.progress}
         style={{ transform: `translateX(${props.translatePosition}px)` }}
       ></div>
-    </div>
-  );
-};
-
-const LeftArrow = (props: { moveLeft: any }) => {
-  return (
-    <div onClick={props.moveLeft}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="35"
-        height="35"
-        viewBox="0 0 35 35"
-        fill="none"
-      >
-        <circle cx="17.5" cy="17.3259" r="17" fill="#005271" />
-        <circle cx="17.5" cy="17.3259" r="17" fill="#005271" />
-        <path
-          d="M19.5 12.3259L14.5 16.6116L19.5 21.3259"
-          stroke="#9EE7E5"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    </div>
-  );
-};
-const RightArrow = () => {
-  return (
-    <div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="35"
-        height="35"
-        viewBox="0 0 35 35"
-        fill="none"
-      >
-        <circle
-          cx="17"
-          cy="17"
-          r="17"
-          transform="matrix(-1 0 0 1 34.5 0.325867)"
-          fill="#005271"
-        />
-        <path
-          d="M15.5 12.3259L20.5 16.6116L15.5 21.3259"
-          stroke="#9EE7E5"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
     </div>
   );
 };
