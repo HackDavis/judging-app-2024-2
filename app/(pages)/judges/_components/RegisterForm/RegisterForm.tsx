f1c17edf2efd279bccdb8029db729c7d25a642a0
Austin Shih <austinhshih@gmail.com>
2024-04-03 22:08:00 -0700

    working register frontend

--- app/(pages)/judges/_components/RegisterForm/RegisterForm.tsx
+++ app/(pages)/judges/_components/RegisterForm/RegisterForm.tsx
@@ -1,38 +1,128 @@
-'use client';
-
-import Register from '@actions/auth/register';
+import { useEffect, useState, ChangeEvent } from 'react';
 import { useFormState } from 'react-dom';
 import { useAuth } from '@hooks/useAuth';
-import { useEffect } from 'react';
+import Register from '@actions/auth/register';
+
+import styles from './RegisterForm.module.scss';
+import Link from 'next/link';
+import AuthTokenInt from '@typeDefs/authToken';
 
 export default function RegisterForm() {
-  const [registerState, RegisterAction] = useFormState(Register, { ok: false });
-  const { user, login } = useAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordDupe, setPasswordDupe] = useState('');
+  const [isValid, setIsValid] = useState(false);
+  const [error, setError] = useState('');
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [passwordDupeError, setPasswordDupeError] = useState(false);
+
+  const [registerState, RegisterAction] = useFormState(Register, {
+    ok: false,
+    body: null,
+    error: null,
+  });
+  const { login } = useAuth();
+
+  const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handlePasswordDupeChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPasswordDupe(event.target.value);
+  };
+
+  const validateForm = (
+    email: string,
+    password: string,
+    passwordDupe: string
+  ) => {
+    // Simple email validation
+    const isEmailValid = /\S+@\S+\.\S+/.test(email) || email.length === 0;
+    if (!isEmailValid) {
+      setError('Email is not valid format.');
+    }
+    setEmailError(!isEmailValid);
+    console.log(!isEmailValid);
+
+    // Password validation (example: minimum length of 6 characters)
+    const isPasswordValid = password.length >= 6 || password.length === 0;
+    if (!isPasswordValid) {
+      setError('Password is too short.');
+    }
+    setPasswordError(!isPasswordValid);
+
+    const passwordMatch =
+      password === passwordDupe || passwordDupe.length === 0;
+    if (!passwordMatch) {
+      setError("Passwords don't match.");
+    }
+    setPasswordDupeError(!passwordMatch);
+
+    // Set isValid state based on email and password validity
+    setIsValid(isEmailValid && isPasswordValid && passwordMatch);
+    if (isEmailValid && isPasswordValid && passwordMatch) {
+      setError('');
+    }
+  };
 
   useEffect(() => {
     if (registerState.ok === true) {
-      login(registerState.body);
+      const user = registerState.body as AuthTokenInt;
+      login(user);
+    } else {
+      const err = registerState.error as string;
+      setError(err);
     }
   }, [registerState, login]);
+
+  useEffect(() => {
+    validateForm(email, password, passwordDupe);
+  }, [email, password, passwordDupe]);
+
   return (
-    <div>
-      <form action={RegisterAction}>
-        <p>Register</p>
-        <div>
-          <label>Email</label>
-          <input name="email" type="email" />
-        </div>
-        <div>
-          <label>Password</label>
-          {/* Change type=text to type=password in the future */}
-          <input name="password" type="text" />
-        </div>
-        <button type="submit">Create an account</button>
-        <p aria-live="polite" className="sr-only">
-          {JSON.stringify(registerState)}
-        </p>
-      </form>
-      <p>{JSON.stringify(user)}</p>
-    </div>
+    <form action={RegisterAction} className={styles.container}>
+      <p className={styles.error_msg}>{error}</p>
+      <div className={styles.fields}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleEmailChange}
+          className={`${emailError ? styles.error : null}`}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={handlePasswordChange}
+          className={`${passwordError ? styles.error : null}`}
+        />
+        <input
+          name="password-dupe"
+          type="password"
+          placeholder="Retype password"
+          value={passwordDupe}
+          onChange={handlePasswordDupeChange}
+          className={`${passwordDupeError ? styles.error : null}`}
+        />
+      </div>
+      <button
+        className={`${styles.login_button} ${isValid ? styles.valid : null}`}
+        type="submit"
+        disabled={!isValid}
+      >
+        Create account
+      </button>
+      <div className={styles.not_judge}>
+        Not a judge? <Link href="/hackers">Click here</Link>
+      </div>
+    </form>
   );
 }
