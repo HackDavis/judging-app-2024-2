dc31fcc172d0ce18ff6885f441f596ce5ec8ddb9
brandonw504 <brandonw504@outlook.com>
2024-04-07 16:30:43 -0700

    Updated weights

--- app/(api)/_utils/scoring/calculateScore.ts
+++ app/(api)/_utils/scoring/calculateScore.ts
@@ -1,3 +1,5 @@
+import tracks from '../../_data/tracks.json' assert { type: 'json' };
+
 export default function calculateScore(scores: object, correlations: object) {
   const finalScore: number = 0;
 
