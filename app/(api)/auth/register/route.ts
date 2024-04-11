04cb0c0ffcd45860504df8993d97a4266c38e0fc
Austin Shih <austinhshih@gmail.com>
2024-04-10 21:48:18 -0700

    Update route.ts

--- app/(api)/auth/register/route.ts
+++ app/(api)/auth/register/route.ts
@@ -6,10 +6,33 @@ import jwt from 'jsonwebtoken';
 import { HttpError } from '@utils/response/Errors';
 import type AuthTokenInt from '@typeDefs/authToken';
 import { Register } from '@datalib/auth/register';
+import { GetManyJudges } from '@datalib/judges/getJudge';
+import getQueries from '@utils/request/getQueries';
+import { verifyHMACSignature } from '@utils/invite/hmac';
 
 export async function POST(request: NextRequest) {
   try {
+    const { data: d, sig: s } = getQueries(request);
+
+    const judgesRes = await GetManyJudges();
+    const judges = await judgesRes.json();
+
+    const verified = verifyHMACSignature(d as string, s as string);
+    if (judges.body?.length !== 0 && !verified) {
+      throw new HttpError('Bad Invite Token');
+    }
+
     const body = await request.json();
+    if (d) {
+      const dd = atob(d);
+      const parsed = JSON.parse(dd);
+      console.log('HELLO');
+      body['email'] = parsed?.email ?? body.email;
+      body['name'] = parsed?.name ?? body.name;
+      body['specialty'] = parsed?.specialty ?? body.specialty;
+      body['role'] = parsed?.role ?? body.role;
+    }
+
     const res = await Register(body);
     const data = await res.json();
 
