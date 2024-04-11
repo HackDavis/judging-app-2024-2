1fd30f01aa64d890a6a25189225b56f02e536234
Austin Shih <austinhshih@gmail.com>
2024-04-10 18:48:45 -0700

    CRUD for submissions tested

--- app/(api)/_datalib/submissions/createSubmission.ts
+++ app/(api)/_datalib/submissions/createSubmission.ts
@@ -6,15 +6,44 @@ import isBodyEmpty from '@utils/request/isBodyEmpty';
 import NoContentError from '@utils/response/NoContentError';
 import HttpError from '@utils/response/HttpError';
 import parseAndReplace from '@utils/request/parseAndReplace';
+import { NotFoundError, DuplicateError } from '@utils/response/Errors';
 
-export const createSubmission = async (body: object) => {
+export const createSubmission = async (body: {
+  judge_id: string;
+  team_id: string;
+}) => {
   try {
     if (isBodyEmpty(body)) {
       throw new NoContentError();
     }
     const parsedBody = await parseAndReplace(body);
-
     const db = await getDatabase();
+
+    const judge = await db.collection('judges').findOne({
+      _id: parsedBody.judge_id,
+    });
+
+    if (judge === null) {
+      throw new NotFoundError(`judge with id: ${body.judge_id} not found.`);
+    }
+
+    const team = await db.collection('teams').findOne({
+      _id: parsedBody.team_id,
+    });
+
+    if (team === null) {
+      throw new NotFoundError(`team with id: ${body.team_id} not found.`);
+    }
+
+    const existingSubmission = await db.collection('submissions').findOne({
+      judge_id: parsedBody.judge_id,
+      team_id: parsedBody.team_id,
+    });
+
+    if (existingSubmission) {
+      throw new DuplicateError('Submission already exists');
+    }
+
     const creationStatus = await db
       .collection('submissions')
       .insertOne(parsedBody);
