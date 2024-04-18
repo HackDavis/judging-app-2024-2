9eaa1ac945fe4347fc59f20bfdfeafa43f7e30e7
Austin Shih <austinhshih@gmail.com>
2024-04-17 22:06:52 -0700

    minimal server actions kept

--- app/(api)/_actions/judges/deleteJudge.ts
+++
@@ -1,9 +0,0 @@
-'use server';
-
-import { DeleteJudge } from '@datalib/judges/deleteJudge';
-import { revalidatePath } from 'next/cache';
-
-export default async function deleteJudge(id: string) {
-  await DeleteJudge(id);
-  revalidatePath('/judges');
-}
