8a73f94debea072fd6e0fb70253fc126cbd5a901
Sauvikesh <saulal@ucdavis.edu>
2024-04-19 11:24:48 -0700

    feat: did most of the styling for the submit comments block

--- app/(pages)/judges/_components/ScoringForm/Comments.tsx
+++ app/(pages)/judges/_components/ScoringForm/Comments.tsx
@@ -1,4 +1,5 @@
 'use client';
+import styles from './Comments.module.scss';
 import { useState } from 'react';
 export default function Comments() {
   const [commentText, setCommentText] = useState('');
@@ -11,15 +12,24 @@ export default function Comments() {
     alert(`The Alert: ${commentText}`);
   };
   return (
-    <div>
-      <h2>Comments</h2>
-      <p>
+    <div className={styles.commentContainer}>
+      <h2 className={styles.commentTitle}>Comments</h2>
+      <p className={styles.commentDescription}>
         Comments help us for deciding tiebreakers. If there was an exceptional
         project or one you’re suspicious of cheating, write it here!
       </p>
-      <input type="text" value={commentText} onChange={onCommentType} />
-      <button onClick={onSubmitComment}>Submit Score</button>
-      <p>Once submitted, results cannot be changed.</p>
+      <input
+        className={styles.commentBox}
+        type="text"
+        value={commentText}
+        onChange={onCommentType}
+      />
+      <button className={styles.submitButton} onClick={onSubmitComment}>
+        Submit Score
+      </button>
+      <p className={styles.submitDescription}>
+        Once submitted, results cannot be changed.
+      </p>
     </div>
   );
 }
