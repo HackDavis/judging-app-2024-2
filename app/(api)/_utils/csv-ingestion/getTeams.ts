aeed4334387f0e0a6b1c9d2a7833c739becb9c02
JayJ104 <123anjalijain@gmail.com>
2024-04-21 01:22:38 -0700

    working parser w caveats

--- app/(api)/_utils/csv-ingestion/getTeams.ts
+++ app/(api)/_utils/csv-ingestion/getTeams.ts
@@ -1,73 +1,72 @@
 import { parse } from 'csv-parse';
 import * as fs from 'fs';
-// import * as path from 'path';
-// import { Result } from 'postcss';
-
-// type TeamSubmission = {
-//   number: number;
-//   name: string;
-//   tracks: string[];
-//   tech_emphasis: number | 1;
-//   design_emphasis: number | 1;
-// };
-
-// const parseCSV = (filePath: string): Promise<any[]> => {
-//   return new Promise((resolve, reject) => {
-//     const results: any[] = [];
-
-//     // Create a read stream from the CSV file
-//     const stream = fs
-//       .createReadStream(filePath)
-//       .pipe(csvParse({ delimiter: ',' }));
-
-//     // Listen for data events
-//     stream.on('data', (data) => {
-//       results.push(data);
-//     });
-
-//     // Listen for the end of the stream
-//     stream.on('end', () => {
-//       resolve(results);
-//     });
-
-//     // Listen for errors
-//     stream.on('error', (error) => {
-//       reject(error);
-//     });
-//   });
-// };
+import { NextResponse } from 'next/server';
+interface parsedRecord {
+  name: string;
+  number: number;
+  tracks: string[];
+}
 
 interface CsvRecord {
-  name: string;
-  numer: number;
-  status: string;
-  tracks: string[];
+  'Project Title': string;
+  'Table Number': string;
+  'Submission Url': string;
+  'Project Status': string;
+  'Judging Status': string;
+  'Highest Step Completed': string;
+  'Project Created At': string;
+  'About The Project': string;
+  '"Try it out" Links': string;
+  'Video Demo Link': string;
+  'Opt-In Prizes': string;
+  '': string;
 }
 
 export default async function GetTeams() {
   const csvFilePath = 'app/(api)/_data/test_projects_2023.csv';
 
   const fileContent = fs.readFileSync(csvFilePath, { encoding: 'utf-8' });
-  // const headers = [
-  //   'Project Title',
-  //   'Table Number',
-  //   'Project Status',
-  //   'Opt-In Prizes',
-  // ];
+  try {
+    const parsePromise = new Promise<parsedRecord[]>((resolve, reject) => {
+      const output: parsedRecord[] = [];
+      parse(
+        fileContent,
+        {
+          delimiter: ',',
+          columns: true,
+        },
+        (error, parsedResult: CsvRecord[]) => {
+          if (error) {
+            console.log('error:');
+            console.error(error.message);
+            reject(error);
+          }
 
-  parse(
-    fileContent,
-    {
-      delimiter: ',',
-      columns: true,
-    },
-    (error, result: CsvRecord[]) => {
-      if (error) {
-        console.log('error:');
-        console.error(error.message);
-        return null;
-      }
-      console.log('Result', result);
-    }
-  );
+          parsedResult.forEach((data: CsvRecord) => {
+            if (data['Table Number'] !== '') {
+              output.push({
+                name: data['Project Title'],
+                number: parseInt(data['Table Number']),
+                tracks: data['Opt-In Prizes']
+                  .split(',')
+                  .filter(
+                    (track) =>
+                      !track.trim().toLowerCase().startsWith('sponsored by')
+                  )
+                  .map((track) => track.trim()),
+              });
+            }
+          });
+          resolve(output);
+        }
+      );
+    });
+
+    const output = await parsePromise;
+
+    return NextResponse.json({ ok: true, body: output, error: null });
+  } catch (e) {
+    const error = e as Error;
+    return NextResponse.json({ ok: false, body: null, error: error.message });
+  }
 }
