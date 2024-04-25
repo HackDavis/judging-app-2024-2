c8ecb9814258ab476ba75c6804b34bfff8dfbe9f
Sauvikesh <saulal@ucdavis.edu>
2024-04-25 01:30:57 -0700

    feat: added view projects button(not working)

--- app/(pages)/judges/_components/JudgingHub/JudgingList.tsx
+++ app/(pages)/judges/_components/JudgingHub/JudgingList.tsx
@@ -21,6 +21,24 @@ export default function JudgingList({
         </p>
       </div>
       <ProjectCarousel projects={projects} />
+      <button className={styles.projects_button}>
+        View All Projects{' '}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="21"
+          height="21"
+          viewBox="0 0 21 21"
+          fill="none"
+        >
+          <path
+            d="M5.25156 11.2527L17.2516 11.2527M17.2516 11.2527L12.3104 7.25269M17.2516 11.2527L12.3104 15.2527"
+            stroke="#173A52"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </button>
     </div>
   );
 }
