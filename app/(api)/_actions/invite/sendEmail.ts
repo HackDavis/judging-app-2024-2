effcbbc34870438e841383489ea9a12523ee74c1
Austin Shih <austinhshih@gmail.com>
2024-04-27 12:54:30 -0700

    mailing confirmation form working

--- app/(api)/_actions/invite/sendEmail.ts
+++ app/(api)/_actions/invite/sendEmail.ts
@@ -1,16 +1,81 @@
 'use server';
 
+import nodemailer from 'nodemailer';
 import FormToJSON from '@utils/form/FormToJSON';
 import { generateHMACSignature } from '@utils/invite/hmac';
+import HttpError from '@utils/response/HttpError';
 
-export default async function sendEmail(prevState: any, formData: FormData) {
-  const data = FormToJSON(formData);
-  data['exp'] =
-    Date.now() +
-    1000 * 60 * (parseInt(process.env.INVITE_TIMEOUT as string) ?? 30);
-  const data_encoded = btoa(JSON.stringify(data));
+const senderEmail = process.env.SENDER_EMAIL;
+const password = process.env.SENDER_PWD;
+const targetEmail = process.env.TARGET_EMAIL;
 
-  const hmac_sig = generateHMACSignature(data_encoded);
-  const hmac_url = `${process.env.BASE_URL}/judges${data.slug}?data=${data_encoded}&sig=${hmac_sig}`;
-  return hmac_url;
+interface Response {
+  ok: boolean;
+  body: string | null;
+  error: string | null;
+}
+
+export default async function sendEmail(
+  prevState: any,
+  formData: FormData
+): Promise<Response> {
+  try {
+    const data = FormToJSON(formData);
+    data['exp'] =
+      Date.now() +
+      1000 * 60 * (parseInt(process.env.INVITE_TIMEOUT as string) ?? 30);
+    const data_encoded = btoa(JSON.stringify(data));
+
+    const hmac_sig = generateHMACSignature(data_encoded);
+    const hmac_url = `${process.env.BASE_URL}/judges${data.slug}?data=${data_encoded}&sig=${hmac_sig}`;
+
+    const { name, email } = data;
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: senderEmail,
+        pass: password,
+      },
+    });
+
+    const msg = `
+      <div class="mail-container">
+        <p>Welcome ${name}</p>
+        <p>Your invite link is: ${hmac_url}</p>
+      </div>
+      <style>
+          .mail_container {
+              display: flex;
+              flex-direction: column;
+              gap: 16px;
+          }
+      </style>
+    `;
+    const mailOptions = {
+      from: `${name} <${senderEmail}>`,
+      to: targetEmail,
+      subject: `HackDavis Judging App Invite`,
+      replyTo: email,
+      html: msg,
+    };
+
+    const output = await new Promise<Response>((resolve, _) => {
+      transporter.sendMail(mailOptions, function (error, _) {
+        if (error) {
+          resolve({ ok: false, body: null, error: error.message });
+        } else {
+          resolve({ ok: true, body: hmac_url, error: null });
+        }
+      });
+    });
+    return output;
+  } catch (e) {
+    const error = e as HttpError;
+    return {
+      ok: false,
+      body: null,
+      error: error.message,
+    };
+  }
 }
--- app/(pages)/judges/admin/_components/InviteLinkForm/InviteLinkForm.tsx
+++ app/(pages)/judges/admin/_components/InviteLinkForm/InviteLinkForm.tsx
@@ -1,42 +1,74 @@
 import { useFormState } from 'react-dom';
-import sendInvite from '@actions/invite/sendInvite';
+// import sendInvite from '@actions/invite/sendInvite';
+import sendEmail from '@actions/invite/sendEmail';
 import styles from './InviteLinkForm.module.scss';
+import FormSubmitConfirmation from '@components/FormSubmitConfirmation/FormSubmitConfirmation';
+import { useEffect, useState } from 'react';
 
 export default function InviteLinkForm() {
-  const [inviteState, inviteAction] = useFormState(sendInvite, null);
+  const [loading, setLoading] = useState(false);
+  const [resStatus, setResStatus] = useState<null | string>(null);
+
+  const [inviteState, inviteAction] = useFormState(sendEmail, {
+    ok: true,
+    body: null,
+    error: null,
+  });
+
+  useEffect(() => {
+    if (inviteState.body || inviteState.error) {
+      setLoading(false);
+      setResStatus(inviteState.ok ? 'success' : 'failed');
+    }
+  }, [inviteState]);
+
+  const handleClose = () => {
+    setLoading(false);
+    setResStatus(null);
+    window.location.reload();
+  };
 
   return (
-    <form action={inviteAction} className={styles.form}>
-      <h3>Invite a Judge</h3>
-      <div className={styles.fields}>
-        <div>
-          <label className={styles.label}>Name</label>
-          <input name="name" type="text" required />
-        </div>
-        <div>
-          <label className={styles.label}>Email</label>
-          <input name="email" type="email" required />
-        </div>
-        <div className={styles.specialty_select}>
-          <label className={styles.label}>Specialty</label>
+    <>
+      <form action={inviteAction} className={styles.form}>
+        <h3>Invite a Judge</h3>
+        <div className={styles.fields}>
           <div>
-            <label>Technical</label>
-            <input type="radio" name="specialty" value="tech" required />
+            <label className={styles.label}>Name</label>
+            <input name="name" type="text" required />
           </div>
           <div>
-            <label>Design</label>
-            <input type="radio" name="specialty" value="design" required />
+            <label className={styles.label}>Email</label>
+            <input name="email" type="email" required />
           </div>
-          <div>
-            <label>General</label>
-            <input type="radio" name="specialty" value="general" required />
+          <div className={styles.specialty_select}>
+            <label className={styles.label}>Specialty</label>
+            <div>
+              <label>Technical</label>
+              <input type="radio" name="specialty" value="tech" required />
+            </div>
+            <div>
+              <label>Design</label>
+              <input type="radio" name="specialty" value="design" required />
+            </div>
+            <div>
+              <label>General</label>
+              <input type="radio" name="specialty" value="general" required />
+            </div>
           </div>
+          <input name="role" type="hidden" value="judge" />
+          <input name="slug" type="hidden" defaultValue="/register" />
         </div>
-        <input name="role" type="hidden" value="judge" />
-        <input name="slug" type="hidden" defaultValue="/register" />
-      </div>
-      <button type="submit">Generate invite link</button>
-      <p className={styles.link_text}>{inviteState ?? ''}</p>
-    </form>
+        <button type="submit" onClick={() => setLoading(true)}>
+          Generate invite link
+        </button>
+        <p className={styles.link_text}>{inviteState.body ?? ''}</p>
+      </form>
+      <FormSubmitConfirmation
+        pending={loading}
+        mailStatus={resStatus}
+        handleClose={handleClose}
+      />
+    </>
   );
 }
