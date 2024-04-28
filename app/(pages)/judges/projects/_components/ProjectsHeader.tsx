d380d828f34a2290ac9393b6d99c107d7eae9118
Sauvikesh <saulal@ucdavis.edu>
2024-04-28 01:52:07 -0700

    feat: added map pop up on all projects page

--- app/(pages)/judges/projects/_components/ProjectsHeader.tsx
+++ app/(pages)/judges/projects/_components/ProjectsHeader.tsx
@@ -3,6 +3,12 @@
 // import { useState } from 'react';
 import styles from './ProjectsHeader.module.scss';
 import { CiMap } from 'react-icons/ci';
+import Image from 'next/image';
+import map from 'public/judges/hub/map.png';
+import { useState } from 'react';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 export default function ProjectsHeader({
   activeTab,
@@ -12,14 +18,42 @@ export default function ProjectsHeader({
   setActiveTab: (tab: number) => void;
 }) {
   // const [activeTab, setActiveTab] = useState(initialTab); // 0: unjudged, 1: scored
+  const [popUpActive, setPopUpActive] = useState(false);
+
+  const handleOpen = () => {
+    setPopUpActive(true);
+  };
+
+  const handleClose = () => {
+    setPopUpActive(false);
+  };
+
+  const boxStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    border: 'none',
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <div className={styles.header_title}>Projects</div>
         <div>
-          <div className={styles.header_map}>
+          <div onClick={handleOpen} className={styles.header_map}>
             <CiMap className={styles.header_map_icon} />
           </div>
+          <Modal open={popUpActive} onClose={handleClose}>
+            <Box sx={boxStyle} onClick={handleClose}>
+              <Image
+                src={map}
+                alt="venue map"
+                width={250}
+                style={{ background: 'black', padding: '20px' }}
+              />
+            </Box>
+          </Modal>
         </div>
       </div>
       <div className={styles.projectType}>
