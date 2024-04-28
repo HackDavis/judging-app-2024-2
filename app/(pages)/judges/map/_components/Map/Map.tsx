76a9f5f55985d2439b78b6f9f5a50499084c0a98
Sauvikesh <saulal@ucdavis.edu>
2024-04-28 14:19:36 -0700

    fixed back arrow

--- app/(pages)/judges/map/_components/Map/Map.tsx
+++ app/(pages)/judges/map/_components/Map/Map.tsx
@@ -7,16 +7,22 @@ import map from 'public/judges/hub/map.png';
 export default function Map() {
   return (
     <div className={styles.container}>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          alignSelf: 'flex-start',
+        }}
+      >
         <Link href="/judges">
           <Image
             src={backArrow}
             alt="back arrow"
             width={50}
-            style={{ marginTop: '8px', marginLeft: '-160px' }}
+            style={{ marginTop: '8px' }}
           />
         </Link>
-        <h3 className={styles.header}>Table Locations</h3>
+        <h3 className={styles.header}>Go Back</h3>
       </div>
 
       <Image src={map} alt={'venue map'} className={styles.map} />
